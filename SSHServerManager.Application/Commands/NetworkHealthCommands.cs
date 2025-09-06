4f5ef0c76c23e1bd831747e9a3daa52d3755598e
Henrique Araújo Neto <anhenrique93@outlook.com>
2025-09-06 19:33:06 +0100

    feat: add network health commands

---
+++ SSHServerManager.Application/Commands/NetworkHealthCommands.cs
@@ -0,0 +1,31 @@
+﻿using SSHServerManager.Application.Interfaces;
+
+namespace SSHServerManager.Application.Commands
+{
+    public record InterfaceIps(IClient Client) : ICommand
+    {
+        public string Execute() => Client.InterfaceIps();
+    }
+
+    public record DefaultRoute(IClient Client) : ICommand
+    {
+        public string Execute() => Client.DefaultRoute();
+    }
+
+    public record ServerDns(IClient Client) : ICommand
+    {
+        public string Execute() => Client.ServerDns();
+    }
+
+    public record NetworkStatistic(IClient Client) : ICommand
+    {
+        public string Execute() => Client.NetworkStatistic();
+    }
+
+    public record SpeedTest(IClient Client) : ICommand
+    {
+        public string Execute() => Client.SpeedTest();
+    }
+
+
+}

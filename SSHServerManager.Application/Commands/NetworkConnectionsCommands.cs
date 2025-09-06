d8daf32babbb1b37d07b5f02fed941a500b1fa32
Henrique Araújo Neto <anhenrique93@outlook.com>
2025-09-06 19:35:36 +0100

    feat: add network connections information commands

---
+++ SSHServerManager.Application/Commands/NetworkConnectionsCommands.cs
@@ -0,0 +1,44 @@
+﻿using SSHServerManager.Application.Interfaces;
+
+namespace SSHServerManager.Application.Commands
+{
+    public record ListeningPorts(IClient Client) : ICommand
+    {
+        public string Execute() => Client.ListeningPorts();
+    }
+
+    public record EstablishedConnections(IClient Client) : ICommand
+    {
+        public string Execute() => Client.EstablishedConnections();
+    }
+
+    public record AllConnections(IClient Client) : ICommand
+    {
+        public string Execute() => Client.AllConnections();
+    }
+
+    public record FirewallStatus(IClient Client) : ICommand
+    {
+        public string Execute() => Client.FirewallStatus();
+    }
+
+    public record FirewallLogs(IClient Client, int lines = 100) : ICommand
+    {
+        public string Execute() => Client.FirewallLogs(lines);
+    }
+
+    public record ICMP(IClient Client) : ICommand
+    {
+        public string Execute() => Client.ICMP();
+    }
+
+    public record DNSResolution(IClient Client, string domain = "google.com") : ICommand
+    {
+        public string Execute() => Client.DNSResolution(domain);
+    }
+
+    public record Traceroute(IClient Client, string domain = "google.com") : ICommand
+    {
+        public string Execute() => Client.Traceroute(domain);
+    }
+}

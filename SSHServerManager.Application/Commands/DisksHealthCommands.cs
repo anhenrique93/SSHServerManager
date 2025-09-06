afd1db0e2a76285acf7e7eecdb7274cd942660bc
Henrique Araújo Neto <anhenrique93@outlook.com>
2025-09-06 19:30:01 +0100

    feat: add disk health commands

---
+++ SSHServerManager.Application/Commands/DisksHealthCommands.cs
@@ -0,0 +1,14 @@
+﻿using SSHServerManager.Application.Interfaces;
+
+namespace SSHServerManager.Application.Commands
+{
+    public record SmartStatus(IClient Client, string disk) : ICommand
+    {
+        public string Execute() => Client.SmartStatus(disk);
+    }
+
+    public record SmartInfo(IClient Client, string disk) : ICommand
+    {
+        public string Execute() => Client.SmartInfo(disk);
+    }
+}
